77f5e7fbbcb3dbeacf7efa5a191b20936ed36cda
Yann Mainier <yann.mainier@gmail.com>
2014-01-04 18:35:13 +0100

    Suppression d info de proxy inutile

--- chef-solo.rb
+++ chef-solo.rb
@@ -1,4 +1,1 @@
 cookbook_path File.expand_path(File.join(File.dirname(__FILE__), "cookbooks"))
-http_proxy "http://proxy.travelcom.michelin-travel.com:8080"
-https_proxy "http://proxy.travelcom.michelin-travel.com:8080"
-no_proxy "localhost,127.0.0.1,*.local,169.254/16,atafu.travelcom.michelin-travel.com,musha.travelcom.michelin-travel.com,mac22"
